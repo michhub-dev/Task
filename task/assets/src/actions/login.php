7598aa7410d0f7486f8a161934f74ab530179602
Michelle David <57120467+michhub-dev@users.noreply.github.com>
2021-03-04 23:38:04 +0100

    First commit

---
+++ task/assets/src/actions/login.php
@@ -0,0 +1,80 @@
+<html class="js-focus-visible" data-js-focus-visible="">
+  <head><meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
+  <style type="text/css">
+@font-face {
+  font-weight: 400;
+  font-style:  normal;
+  font-family: 'Circular-Loom';
+
+  src: url('https://cdn.loom.com/assets/fonts/circular/CircularXXWeb-Book-cd7d2bcec649b1243839a15d5eb8f0a3.woff2') format('woff2');
+}
+
+@font-face {
+  font-weight: 500;
+  font-style:  normal;
+  font-family: 'Circular-Loom';
+
+  src: url('https://cdn.loom.com/assets/fonts/circular/CircularXXWeb-Medium-d74eac43c78bd5852478998ce63dceb3.woff2') format('woff2');
+}
+
+@font-face {
+  font-weight: 700;
+  font-style:  normal;
+  font-family: 'Circular-Loom';
+
+  src: url('https://cdn.loom.com/assets/fonts/circular/CircularXXWeb-Bold-83b8ceaf77f49c7cffa44107561909e4.woff2') format('woff2');
+}
+
+@font-face {
+  font-weight: 900;
+  font-style:  normal;
+  font-family: 'Circular-Loom';
+
+  src: url('https://cdn.loom.com/assets/fonts/circular/CircularXXWeb-Black-bf067ecb8aa777ceb6df7d72226febca.woff2') format('woff2');
+}</style></head>
+  <body>
+    <div class="kr-loading-fnc">
+      <div> <div class="sk-folding-cube sk-folding-cube-orange"> <div class="sk-cube1 sk-cube"></div> <div class="sk-cube2 sk-cube"></div> <div class="sk-cube4 sk-cube"></div> <div class="sk-cube3 sk-cube"></div> </div> </div>
+    </div>
+    <header>
+      <img src="./5fabede84ca91-2611-Asset 28white background - link invest.png" alt="">
+    </header>
+    <section class="kr  -login-act" act="login.php">
+      <section class="kr-login-field">
+        <div class="kr-login-field-svl">
+          <div class="kr-login-inpt">
+            <input type="email" class="kr-login-emailcontroller" name="kr_usr_email" placeholder=" " autofocus="" value="">
+            <span>Your e-mail address</span>
+          </div>
+          <div class="kr-login-inpt">
+            <input type="password" class="kr-login-inpt-nsthr" name="kr_usr_pwd" placeholder=" " value="">
+            <span>Your password</span>
+          </div>
+        </div>
+        <footer>
+          <a class="kr-resetpassword-view">Forgot password ?</a>
+          <button class="g-recaptcha btn-shadow" data-sitekey="" data-size="invisible" data-callback="kryptoLogin">LOGIN</button>
+        </footer>
+      </section>
+        <section class="kr-login-separator">
+        <div></div>
+        <span>or</span>
+        <div></div>
+      </section>
+      <section class="kr-login-oauth">
+      <div class="">
+                  </div>
+            <a class="btn-shadow btn-black kr-login-signup-ctrl">
+          <div class="kr-login-oauth-name">
+            Create a new account        </div>
+        </a>
+        </section>
+    </section>
+  </body>
+  <loom-container id="lo-engage-ext-container">
+    <div></div>
+    <loom-shadow classname="resolved">
+
+    </loom-shadow>
+  </loom-container>
+</html>
